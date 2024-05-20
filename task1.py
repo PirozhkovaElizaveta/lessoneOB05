8b442c3ec8576f973a939e99d6d3695554303e21
PirozhkovaElizaveta <zhenshina21@mail.ru>
2024-05-20 17:51:20 +0300

    add part of code

---
+++ task1.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+window_size = (800, 600)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Тестовый проект")
+
+image = pygame.image.load("Название изображения")
+image_rect = image.get.rect()
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
